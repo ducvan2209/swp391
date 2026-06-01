a952a68e5de2bf77ff70d85d02f80cfb7454e519
ducvan <ducbeo2209@gmail.com>
2026-06-01 23:34:07 +0700

    color

--- MakeHash.java
+++
@@ -1,7 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-
-public class MakeHash {
-    public static void main(String[] args) {
-        System.out.println(BCrypt.hashpw("123456", BCrypt.gensalt(12)));
-    }
-}
